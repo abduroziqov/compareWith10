d3d27fbccb2d6df7dcadf7e53ffe294ddf7ee4d8
abdurozikov.kuvonchbek@gmail.com <abdurozikov.kuvonchbek@gmail.com>
2023-03-19 23:31:41 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace compareWith10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Berilgan son 10 dan katta bo'lsa unga 3 ni qo'shib konsolga chiqaring.
+            //Agar 10 dan kichik bo'lsa 2ga ko'paytirib natijani konsolga chiqaring.
+            //Agar 10 ga teng bo'lsa 22 sonini konsolga chiqaring.
+
+            while (true)
+            {
+                Console.Write("Sonni kiriting => ");
+                int son = Convert.ToInt32(Console.ReadLine());
+
+                if (son > 10)
+                {
+                    Console.WriteLine($"{son + 3}");
+                }
+                else if (son < 10)
+                {
+                    Console.WriteLine($"{son * 2}");
+                }
+                else
+                {
+                    Console.WriteLine($"{son + 12}");
+                }
+            }
+        }
+    }
+}
